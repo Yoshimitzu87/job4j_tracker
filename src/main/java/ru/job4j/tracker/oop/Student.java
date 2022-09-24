45b2d43a34e2b93be532eeaae8a06eb121167413
Yoshimitzu87 <spitfires111@gmail.com>
2022-09-24 20:34:57 +0300

    1.2. Вызов метода объекта [#166085]

--- src/main/java/ru/job4j/tracker/oop/Student.java
+++ src/main/java/ru/job4j/tracker/oop/Student.java
@@ -9,6 +9,7 @@ public class Student {
         System.out.println("I believe i can fly");
     }
 
+
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
