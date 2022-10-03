f0d25b8c976f1bf1974884c23f22924d4d2bc782
Yoshimitzu87 <spitfires111@gmail.com>
2022-10-03 19:22:20 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,40 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean firstError;
+    private int secondError;
+    private String thirdError;
+
+    public Error(boolean firstError, int secondError, String thirdError) {
+        this.firstError = firstError;
+        this.secondError = secondError;
+        this.thirdError = thirdError;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("ПерваяОшибка: " + firstError);
+        System.out.println("ВтораяОшибка: " + secondError);
+        System.out.println("ТретьяОшибка: " + thirdError);
+    }
+
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 2, "asdasd");
+        error.printInfo();
+        System.out.println(" ");
+        Error errorOne = new Error();
+        errorOne.printInfo();
+        System.out.println(" ");
+        Error errorTwo = new Error(false, 22, "asasd");
+        errorTwo.printInfo();
+    }
+}
+
+
+
+
+
