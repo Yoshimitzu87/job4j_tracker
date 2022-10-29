a93adf4bcb7b46d4de77dc4d52f8870ebae62509
Yoshimitzu87 <spitfires111@gmail.com>
2022-10-29 03:43:01 +0300

    8. Реализация меню за счет шаблона стратегия.

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class FindByNameAction implements UserAction{
+public class FindByNameAction implements UserAction {
     @Override
     public String name() {
         return "Find by Name";
