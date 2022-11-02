a8029ab84fa12840d1dfb778243da49958109e17
Yoshimitzu87 <spitfires111@gmail.com>
2022-11-02 11:05:36 +0300

    9.2. Тесты вывода на консоль [#33585 #356484]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -89,6 +89,8 @@ public class StartUITest {
                 "Menu." + ln
                         + "0. Edit Item" + ln
                         + "1. Exit" + ln
+                        + "=== Edit item ===" + ln
+                        + "Заявка изменена успешно." + ln
                         + "Menu." + ln
                         + "0. Edit Item" + ln
                         + "1. Exit" + ln
@@ -113,6 +115,7 @@ public class StartUITest {
                 "Menu." + ln
                         + "0. Show All Items" + ln
                         + "1. Exit" + ln
+                        + one + ln
                         + "Menu." + ln
                         + "0. Show All Items" + ln
                         + "1. Exit" + ln
@@ -137,6 +140,8 @@ public class StartUITest {
                 "Menu." + ln
                         + "0. Find Item" + ln
                         + "1. Exit" + ln
+                        + "=== Find item by id ===" + ln
+                        + one + ln
                         + "Menu." + ln
                         + "0. Find Item" + ln
                         + "1. Exit" + ln
@@ -149,7 +154,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(
-                new String[]{"0", String.valueOf(one.getName()), "1"}
+                new String[]{"0", one.getName(), "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new FindByNameAction(out),
@@ -161,6 +166,8 @@ public class StartUITest {
                 "Menu." + ln
                         + "0. Find by Name" + ln
                         + "1. Exit" + ln
+                        + "=== Find items by name ===" + ln
+                        + one + ln
                         + "Menu." + ln
                         + "0. Find by Name" + ln
                         + "1. Exit" + ln
