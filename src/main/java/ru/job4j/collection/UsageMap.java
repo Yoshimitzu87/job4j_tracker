369baae6f914e657b265707d22c2a6c463df338f
Yoshimitzu87 <spitfires111@gmail.com>
2023-02-04 09:53:18 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("afro@yandex.ru", "Anton Frolov");
+        for (String key : map.keySet()){
+            System.out.println(map.get(key));
+        }
+    }
+}
+
