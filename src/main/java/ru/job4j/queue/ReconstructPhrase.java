108001157d11081539cf1a286f33ff0c60fa8ed8
Yoshimitzu87 <spitfires111@gmail.com>
2023-02-04 07:23:31 +0300

    1. Интерфейс Dequeue [#504942]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,42 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+import java.util.Iterator;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; evenElements.size() > 0; i++) {
+            if (i % 2 == 0) {
+                builder.append(evenElements.pollFirst());
+            }
+            else {
+                evenElements.pollFirst();
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder builder = new StringBuilder();
+        Iterator<Character> iterator = descendingElements.descendingIterator();
+        while (iterator.hasNext()) {
+            builder.append(iterator.next());
+        }
+        return builder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
